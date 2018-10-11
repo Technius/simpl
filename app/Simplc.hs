b328bdb0641c9aaef7c73a2fd0aa8457e8b19a54
Bryan Tan <techniux@gmail.com>
2018-10-11 00:04:26 -0700

    Display LLVM IR when generating code (for debugging)

--- app/Simplc.hs
+++ app/Simplc.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE LambdaCase #-}
 module Simplc where
 
+import qualified Data.ByteString as B
 import Control.Exception
 import Control.Monad (forM_)
 import Data.List (find)
@@ -10,7 +11,7 @@ import Simpl.Ast
 import Simpl.Compiler
 import qualified Simpl.Parser as Parser
 import LLVM.Target (withHostTargetMachine)
-import LLVM.Module (File(File), withModuleFromAST, writeObjectToFile)
+import LLVM.Module (File(File), withModuleFromAST, writeObjectToFile, moduleLLVMAssembly)
 import LLVM.Internal.Context
 import LLVM.Analysis (verify)
 import qualified Data.Text as Text
@@ -28,6 +29,7 @@ codegen :: SourceFile Expr -> IO ()
 codegen srcFile@(SourceFile _ decls) =
   case find isMain decls of
     Just _ -> do
+      putStrLn "Running compiler pipeline"
       modRes <- fullCompilerPipeline srcFile
       case modRes of
         Left err -> putStrLn $ "Error: " ++ show err
@@ -57,5 +59,9 @@ outputModule llvmMod =
   withHostTargetMachine $ \target ->
     withContext $ \ctx ->
       withModuleFromAST ctx llvmMod $ \mod' -> do
+        putStrLn "Generated IR"
+        moduleLLVMAssembly mod' >>= B.putStr
+        putStrLn "Verifying module"
         verify mod'
+        putStrLn "Generating code"
         writeObjectToFile target (File "sample.o") mod'
