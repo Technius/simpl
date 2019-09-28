35cad7c846cbb55952c65dbd67a08f4fe7cbb8e5
Bryan Tan <techniux@gmail.com>
2019-09-28 00:13:00 -0700

    Add new WIP test script

--- test.sh
+++ test.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 set -e
 COMPILER_ARGS="$@"
-FILENAME="sample"
 stack build --fast
-stack exec simplc -- "$FILENAME.spl" $COMPILER_ARGS
 make -C runtime libgc.a
-clang -pthread runtime/libgc.a "$FILENAME.o" -o "$FILENAME" -lm
-./$FILENAME
+
+TEST_DIR=test-suite
+TEST_BIN_DIR="$TEST_DIR/bin"
+mkdir -p $TEST_BIN_DIR
+
+for src_file in $(find "$TEST_DIR" -type f -name '*.spl'); do
+    out_name=$(basename "${src_file%.spl}.o")
+    out_name="$TEST_BIN_DIR/$out_name"
+    echo "Building ${out_name%.o}..."
+    stack exec simplc -- "$src_file" -o "$out_name" $COMPILER_ARGS
+    clang -g -pthread runtime/libgc.a "$out_name" -o "${out_name%.o}" -lm
+done
